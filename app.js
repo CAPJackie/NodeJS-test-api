59ac7057409ff4f6ce2283ef470cc5fba786f4be
Juan David Ramirez <JuanDavid.Mendoza@cshs.org>
2022-08-23 09:14:08 -0500

    Updated app with route handlers

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const campaignRoutes = require('./routes/campaignRoutes');
+const contactsRoutes = require('./routes/contactRoutes');
+const AppError = require('./utils/appError');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/campaigns', campaignRoutes);
+app.use('/api/v1/contacts', contactsRoutes);
 
-// TODO: Write routes
+app.all('*', (req, _, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
